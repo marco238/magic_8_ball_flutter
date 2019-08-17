275e4e9ff42169237249543fbe7e979056b78530
Marco <ma.monzon@soprasteria.com>
2019-08-17 23:15:28 +0200

    feat: magic ball working fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ask me anything 🎱',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int answerNumber = 1;
+
+  void getAnswer() {
+    setState(() {
+      answerNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          splashColor: Colors.white,
+          highlightColor: Colors.white,
+          onPressed: getAnswer,
+          child: Image(
+            image: AssetImage('images/ball$answerNumber.png'),
+          ),
+        ),
       ),
     );
+  }
+}
